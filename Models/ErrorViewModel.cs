f88856b8fec8d5de8ea8f61c38acaf875a02fe72
Wesley Reisz <wes@wesleyreisz.com>
2021-05-03 18:17:07 -0400

    adding initial files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sample_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
